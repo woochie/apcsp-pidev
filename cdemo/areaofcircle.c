ec4e4977728e77eaa90a368c02e6eb2cde9d3915
woochie <50209769+woochie@users.noreply.github.com>
2019-05-27 09:45:55 -0700

    Create areaofcircle.c

---
+++ cdemo/areaofcircle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+float areaOfCircle(float rad)
+{
+  float area = 3.14 * rad * rad;
+  return area;
+}
+
+int main()
+{
+  float a;
+  for(float rad = 3.5; rad <= 12.5; rad++)
+  {
+    a = areaOfCircle(rad);
+    printf("The circle that has a radius of %f has an area that is equal to %f\n", rad, a);
+  
+  }
+
+}
